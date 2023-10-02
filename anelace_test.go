d779e25b1d1455afe899e1971378c68d8a89408d
Jay Logelin <lodge@protocol.ai>
2023-10-02 14:34:54 +0000

    removed superflous read stream optimizer from determinism test, renamed test

--- anelace_test.go
+++ anelace_test.go
@@ -10,7 +10,6 @@ import (
 	"testing"
 
 	"github.com/anjor/anelace"
-	"github.com/anjor/anelace/internal/util/stream"
 )
 
 func TestDeterministicCarContent(t *testing.T) {
@@ -19,13 +18,7 @@ func TestDeterministicCarContent(t *testing.T) {
 
 	var first [32]byte
 	for iter := 0; iter < TEST_ITERATIONS; iter++ {
-		inStat, statErr := os.Stdin.Stat()
-		if statErr != nil {
-			t.Fatalf("unexpected error stat-ing stdin: %s", statErr)
-		}
-
 		mockStderr, mockStdout := new(bytes.Buffer), new(bytes.Buffer)
-
 		anl, errs := anelace.NewAnelaceWithWriters(mockStderr, mockStdout)
 		if len(errs) > 0 {
 			for _, err := range errs {
@@ -35,19 +28,13 @@ func TestDeterministicCarContent(t *testing.T) {
 		}
 
 		var err error
-
 		//mockOsStdin, err := os.CreateTemp("", "*")
 		mockOsStdin, err := os.Open("/Users/jay/Documents/code/anelace/bin/sample.txt")
 		if err != nil {
 			fmt.Printf("Error: %s", err)
 		}
 
-		for _, opt := range stream.ReadOptimizations {
-			if err := opt.Action(mockOsStdin, inStat); err != nil && err != os.ErrInvalid {
-				log.Printf("Failed to apply read optimization hint '%s' to stdIN: %s\n", opt.Name, err)
-			}
-		}
-
+		// process the mock stdin input
 		processErr := anl.ProcessReader(
 			mockOsStdin,
 			nil,
@@ -57,6 +44,7 @@ func TestDeterministicCarContent(t *testing.T) {
 			log.Fatalf("Unexpected error processing STDIN: %s", processErr)
 		}
 
+		// check to see if the sums match
 		if iter == 0 {
 			first = sha256.Sum256(mockStdout.Bytes())
 		} else {
