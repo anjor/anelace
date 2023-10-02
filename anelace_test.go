d5cead3f29cd2426a4e3188b1be0ab4118eecc37
Jay Logelin <lodge@protocol.ai>
2023-10-02 14:58:18 +0000

    scaffolding for future multi-content tests

--- anelace_test.go
+++ anelace_test.go
@@ -12,41 +12,51 @@ import (
 
 func TestDeterministicCarContent(t *testing.T) {
 
+	deterministicTests := []struct {
+		testContent string
+	}{
+		{
+			testContent: "test/sample-payload.dat",
+		},
+	}
+
 	const TEST_ITERATIONS = 10
+	for _, tt := range deterministicTests {
 
-	var first [32]byte
-	for iter := 0; iter < TEST_ITERATIONS; iter++ {
-		mockStderr, mockStdout := new(bytes.Buffer), new(bytes.Buffer)
-		anl, errs := NewAnelaceWithWriters(mockStderr, mockStdout)
-		if len(errs) > 0 {
-			for _, err := range errs {
-				fmt.Println(err)
-				t.Error(err)
+		var first [32]byte
+		for iter := 0; iter < TEST_ITERATIONS; iter++ {
+			mockStderr, mockStdout := new(bytes.Buffer), new(bytes.Buffer)
+			anl, errs := NewAnelaceWithWriters(mockStderr, mockStdout)
+			if len(errs) > 0 {
+				for _, err := range errs {
+					fmt.Println(err)
+					t.Error(err)
+				}
 			}
-		}
 
-		mockOsStdin, err := os.Open("test/sample-payload.dat")
-		if err != nil {
-			fmt.Printf("Error: %s", err)
-		}
+			mockOsStdin, err := os.Open(tt.testContent)
+			if err != nil {
+				fmt.Printf("Error: %s", err)
+			}
 
-		// process the mock stdin input
-		processErr := anl.ProcessReader(
-			mockOsStdin,
-			nil,
-		)
-		anl.Destroy()
-		if processErr != nil {
-			log.Fatalf("Unexpected error processing STDIN: %s", processErr)
-		}
+			// process the mock stdin input
+			processErr := anl.ProcessReader(
+				mockOsStdin,
+				nil,
+			)
+			anl.Destroy()
+			if processErr != nil {
+				log.Fatalf("Unexpected error processing STDIN: %s", processErr)
+			}
 
-		// check to see if the sums match
-		if iter == 0 {
-			first = sha256.Sum256(mockStdout.Bytes())
-		} else {
-			current := sha256.Sum256(mockStdout.Bytes())
-			if current != first {
-				t.Errorf("iteration %d: content sum does not match first content sum on iteration [ %s, %s ]", iter, hex.EncodeToString(first[:]), hex.EncodeToString(current[:]))
+			// check to see if the sums match
+			if iter == 0 {
+				first = sha256.Sum256(mockStdout.Bytes())
+			} else {
+				current := sha256.Sum256(mockStdout.Bytes())
+				if current != first {
+					t.Errorf("iteration %d: content sum does not match first content sum on iteration [ %s, %s ]", iter, hex.EncodeToString(first[:]), hex.EncodeToString(current[:]))
+				}
 			}
 		}
 	}
